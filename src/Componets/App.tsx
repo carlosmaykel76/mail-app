6231e605c845a63839c20eea3169e394f63dfacd
Carlos Maykel Lopez Gonzalez <cmlg.cu@gmail.com>
2021-06-18 16:47:57 -0400

    Adecuando el proyecto

---
+++ src/Componets/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Styles/App.css'
+
+const App = () => {
+  
+  return (
+
+    <h2>esto esta malo</h2>  
+
+  );
+
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './Styles/index.css';
+import App from './Componets/App';
 
 ReactDOM.render(
   <React.StrictMode>
