bae12ea528bc97a7dcfa5ba3a2de495ce326e6a7
Carlos Maykel <cmlg.cu@gmail.com>
2021-07-07 15:31:18 -0400

    Elimina Comp y funciona el onclick y el onselect

--- src/Componets/Inbox.tsx
+++ src/Componets/Inbox.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Splitter } from "@progress/kendo-react-layout";
 
 import ToolBar from  './Layout/ToolBar';
 import ComponseForm from './Modals/ComponseForm';
-import MailSplitter from './Layout/MailSplitter'
+import MessagesList from '../Componets/Mail/MessagesList/MessagesList';
+import MessageDetails from '../Componets/Mail/MessageDetails/MessageDetails';
+import MessageData from '../data/MessageData'
 
 import '../Styles/App.css'
+
+interface IMessage {
+  id: number;
+  importance: string;
+  attached: boolean;
+  personfor: string;
+  subject: string;
+  sent: string;
+  size: string;
+  read: boolean
+}
 
 /*const split = {[
    collapsed?: boolean,
@@ -18,23 +32,66 @@ import '../Styles/App.css'
 ]},*/
 
 const Inbox = (Props: any) => {
-
   const [visible, setVisible] = React.useState(false);
+  const [itemSelectState, setItemSelectState] = useState<IMessage[]>([]);
 
   const toggleDialog = () => {
       setVisible(!visible);
   }
+  
+  //const CountMsg = MessageData.length;
+
+  const [panes, setPanes] = useState([
+    { size: "15%", min: "150px",  collapsible: false },
+    {},
+    { size: "30%", min: "20px", collapsible: true },
+  ]); 
+
+  const onChange = (event:any) => {
+    setPanes(event.newState);
+  };
+
+  const handleClickSelectMsg = (event: React.MouseEvent<unknown>, id:number) => {
+    //const isItemSelected = true;
+    alert('Esto id del msg '+ id);
+  }; 
+
+  const handleSelect = (event: React.MouseEvent<unknown>, id:number) => {
+
+    let data = MessageData.find(msg => msg.id = id);
+
+    setItemSelectState(data)
+
+    alert(data?.subject);
+  }
 
   return (
     <>
-      <ToolBar openCompose={toggleDialog}/>
-      <div>
+      <ToolBar openCompose={toggleDialog}/>  
+
+      <Splitter style={{ height: 500 }} panes={panes} onChange={onChange}>
+        <div className="pane-content">
+            <h3>Inner splitter / left pane</h3>
+        </div>
+        <div className="pane-content">               
+            <MessagesList 
+              dataList={MessageData} 
+              onClick={handleClickSelectMsg} 
+              onSelect={handleSelect}              
+            />
+        </div>
+        <div className="pane-content">
+            <MessageDetails />
+        </div>
+      </Splitter>  
+
+       <div>
         {visible &&
          <ComponseForm openCompose={toggleDialog} />
         }
-      </div>
+      </div>   
 
-      <MailSplitter />
+      
     </>
   );
 }
