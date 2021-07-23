a146aad51e1abe46f960509dd601c8265d1e0720
Carlos Maykel <cmlg.cu@gmail.com>
2021-07-23 12:16:03 -0400

    corecciones menores

--- src/Componets/Layout/ToolBar.tsx
+++ src/Componets/Layout/ToolBar.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import EmailIcon from "@material-ui/icons/Email";
 import DeleteIcon from "@material-ui/icons/Delete";
 import SettingsIcon from "@material-ui/icons/Settings";
-import MarkAsUnreadIcon from "@material-ui/icons/MarkunreadMailbox";
-import { ButtonGroup, Button, IconButton } from "@material-ui/core";
+import { ButtonGroup, IconButton } from "@material-ui/core";
 import Tooltip from "@material-ui/core/Tooltip";
 import { makeStyles } from "@material-ui/core/styles";
 
